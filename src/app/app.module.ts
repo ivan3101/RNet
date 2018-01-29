52b1aeacde83026dfd5d5565783332b5233dfd71
Ivan De Menezes <ivandmenezes@gmail.com>
2018-01-28 23:26:32 -0400

    Added news-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NewsComponent } from './news/news.component';
+import { NewsListComponent } from './news/news-list/news-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NewsComponent
+    NewsComponent,
+    NewsListComponent
   ],
   imports: [
     BrowserModule
