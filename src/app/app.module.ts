888f76efa1f291dd8338aadde8b944bea8c70a80
Ivan De Menezes <ivandmenezes@gmail.com>
2018-01-28 23:21:40 -0400

    Added news component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NewsComponent } from './news/news.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule
