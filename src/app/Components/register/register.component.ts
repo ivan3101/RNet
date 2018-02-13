83c310c538b1685b7dc015f16254321d446de8fc
Ivan De Menezes <ivandmenezes@gmail.com>
2018-02-13 15:36:56 -0400

    Register added

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
+  registered: boolean;
+  registerForm: FormGroup;
+  constructor() {
+    this.registered = false;
+  }
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      'fullname': new FormGroup({
+        'firstname': new FormControl(null, [Validators.required]),
+        'lastname': new FormControl(null, [Validators.required])
+      }),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'username': new FormControl(null, [Validators.required]),
+      'password': new FormGroup({
+        'newPassword': new FormControl(null, [Validators.required]),
+        'confirmPassword': new FormControl(null, [Validators.required])
+      })
+    });
+  }
+  onSubmit() {
+    console.log(this.registerForm);
+    this.registered = true;
+    this.registerForm.reset();
   }
 
 }
