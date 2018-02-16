7c0867089f2f90c6cd337bdaa40fd544a51435d2
Ivan De Menezes <ivandmenezes@gmail.com>
2018-02-16 03:22:11 -0400

    fixes

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,28 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {ArticleService} from '../../Services/article.service';
-import {animate, keyframes, query, stagger, style, transition, trigger} from '@angular/animations';
+import {animate, keyframes, query, stagger, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  animations: [
-    trigger('goals',[
-      transition('* => *', [
-        query(':enter', style({ opacity: 0}), {optional: true}),
-        query(':enter', stagger('300ms', [
-          animate('.6s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
-            style({opacity: 1, transform: 'translateY(0)', offset: 1.0}),
-          ]))
-        ]), {optional: true} )
-      ])
-    ])
-  ]
 })
 export class HomeComponent implements OnInit {
-
   articles;
   constructor(private articleServie: ArticleService) {  }
   ngOnInit() {
